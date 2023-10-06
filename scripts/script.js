75db1b6b0673eb9fa24cc805c55dd36183751447
Orange Workbook <d.riley0189@gmail.com>
2023-10-06 17:07:48 +0100

    finished pc site design + functionality

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+
+// mouseover ref-  https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onmouseover
+
+
+//   const defaultImg = (x) => {
+//     x.style.background-color ="orangered";
+//     x.style.background-blend-mode = "multiply";
+//   };
+
+//   .work-background-img-child.onmouseover = const onHoverImg =(x) => {
+//     x.style.background-color = "orangered";
+//     x.style.background-blend-mode = "lighten";
+//   };
